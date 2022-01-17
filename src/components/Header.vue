f69205582574fa03b227145690bba85e86ef535a
Valkyrux <philadephos.granata@gmail.com>
2022-01-17 15:53:19 +0100

    main-components

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+      <img src="../assets/logo-spotify-icon-4096.png" alt="spotify logo">
+  </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/partials/_commons.scss";
+header {
+    height: 100px;
+    background-color: $gray;
+}
+
+img {
+    width: 50px;
+    text-align: start;
+}
+</style>
